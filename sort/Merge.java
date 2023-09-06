1fdb89be925646dd427efa442ca6dd79cf7a36b6
Victor Castro II <victor.castro ii@mrjaffesclass.com>
2023-09-06 12:45:21 -0700

    adslkjfasl

--- sort/Merge.java
+++ sort/Merge.java
@@ -10,45 +10,43 @@ import java.util.Arrays;
  */
 public class Merge
 {
-  
-  /**
-   * Returns the sorted array
-   */
-  public static int[] sort(int[] arr) {
-    int N = arr.length;
-    while (N > 1) {
-        int middle = N/2;
-        int leftLength = middle;
-        int rightLength = N - leftLength;
-        int index = 0;
-        
-        if (index < middle) {
-            left[index] = arr[index];
-            index++;
+
+    /**
+     * Returns the sorted array
+     */
+    void merge(int a[], int beg, int mid, int end) {
+        int i, j, k;
+        int n1 = mid - beg + 1;
+        int n2=end-mid;
+        int LeftArray[n1];
+        int RightArray[n2];
+
+        for (int i = 0; i < n1; i++) {
+            LeftArray[i] = a[beg+1];
+        }
+        for (int j = 0; j < n2; j++) {
+            RightArray[j] = a[mid + 1 +j];
         }
     }
-    return arr;
-  }
 
-  
-  public static void main(String[] args) {
-    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
-      31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
-      73,54,51,25,33,20,52,79,97,70,54,63,49};    
-    
-    // Test the sort
-    testSort(sort(arr));
-  }
-  
-  public static void testSort(int[] arr) {
-    for (int i=0; i<arr.length-1; i++) {
-      if (arr[i] > arr[i+1]) {
-        System.out.println("FAIL at index "+i);
-        System.out.println(Arrays.toString(arr));
-        return;
-      }
+    public static void main(String[] args) {
+        int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+                31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+                73,54,51,25,33,20,52,79,97,70,54,63,49};    
+
+        // Test the sort
+        testSort(sort(arr));
     }
-    System.out.println("SUCCESS!");
-  }
+
+    public static void testSort(int[] arr) {
+        for (int i=0; i<arr.length-1; i++) {
+            if (arr[i] > arr[i+1]) {
+                System.out.println("FAIL at index "+i);
+                System.out.println(Arrays.toString(arr));
+                return;
+            }
+        }
+        System.out.println("SUCCESS!");
+    }
 
 }
