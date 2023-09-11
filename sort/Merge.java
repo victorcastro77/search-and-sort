cbd7e139a0a353a44481f435ee616130f48397a1
Victor Castro II <victor.castro ii@mrjaffesclass.com>
2023-09-11 12:49:56 -0700

    asdlkjf

--- sort/Merge.java
+++ sort/Merge.java
@@ -12,55 +12,26 @@ import java.util.Set;
  */
 public class Merge
 {
-
-    /**
-     * Returns the sorted array
-     */
-    public static void mergeSort(int[] a, int n) {
-        if (n < 2) {
-            return;
-        }
-        int mid = n/2;
-        int[] l = new int[mid];
-        int[] r = new int[n-mid];
-
-        for (int i = 0; i < mid; i++) {
-            l[i] = a[i];
-        }
-        for (int i = mid; i < n; i++) {
-            r[i-mid]=a[i];
-        }
-        mergeSort(l, mid);
-        mergeSort(r, n - mid);
-        merge(a, l, r, mid, n - mid);
+    public static int[] sort(int[] arr) {
+        
     }
-
-    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
-        int i = 0, j = 0, k = 0;
-        while (i < left && j < right) {
-            if (l[i] <= r[j]) {
-                a[k++] = l[i++];
-            }
-            else {
-                a[k++] = r[j++];
-            }
-        }
-        while (i < left) {
-            a[k++] = r[j++];
-        }
-        while (j < right) {
-            a[k++] = r[j++];
-        }
-
-    }
-
+    public static void main(String[] args) {
+    int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
+      31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
+      73,54,51,25,33,20,52,79,97,70,54,63,49};    
     
-    public void positiveTest() {
-        int[] actual = { 5, 1, 6, 2, 3, 4 };
-        int[] expected = { 1, 2, 3, 4, 5, 6 };
-        mergeSort(actual, actual.length);
-        assertArrayEquals(expected, actual);
+    // Test the sort
+    testSort(sort(arr));
+  }
+  
+  public static void testSort(int[] arr) {
+    for (int i=0; i<arr.length-1; i++) {
+      if (arr[i] > arr[i+1]) {
+        System.out.println("FAIL at index "+i);
+        System.out.println(Arrays.toString(arr));
+        return;
+      }
     }
-
-    
+    System.out.println("SUCCESS!");
+  }
 }
