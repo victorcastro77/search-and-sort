5853e948ff5f8d7aa25401c34ee1d194c9959b23
Victor-Castro <victor.castro@mrjaffesclass.com>
2023-09-11 17:34:19 -0700

    adsljf

--- sort/Merge.java
+++ sort/Merge.java
@@ -1,8 +1,6 @@
 package sort;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Write a description of class Merge here.
@@ -12,10 +10,61 @@ import java.util.Set;
  */
 public class Merge
 {
-    public static int[] sort(int[] arr) {
-        
+  
+  /**
+   * Returns the sorted array
+   */
+  public static int[] sort(int[] arr) {
+    int inputLength = arr.length;
+    
+    if (inputLength < 2) {
+        return arr;
     }
-    public static void main(String[] args) {
+    int mid = inputLength/2;
+    int[] l = new int[mid];
+    int[] r = new int[inputLength - mid];
+    for (int i = 0; i < mid; i++) {
+        l[i]=arr[i];
+    }
+    for (int i = mid; i < inputLength; i++) {
+        r[i-mid]=arr[i];
+    }
+    
+    sort(l);
+    sort(r);
+    
+    merge(arr, l, r);
+    return arr;
+  }
+  
+  private static void merge (int[] arr, int[] l, int[]r) {
+      int leftSize = l.length;
+      int rightSize = r.length;
+      int i = 0, j = 0, k = 0;
+      
+      while(i < leftSize && j < rightSize) {
+          if (l[i] <= r[i]) {
+              arr[k] = l[i];
+              i++;
+          }
+          else {
+              arr[k] = r[j];
+          }
+          k++;
+      }
+      while (i < leftSize) {
+          arr[k] = l[i];
+          i++;
+          k++;
+      }
+      while (j < rightSize) {
+          arr[k] = l[j];
+          j++;
+          k++;
+      }
+  }
+  
+  public static void main(String[] args) {
     int[] arr = {53,85,93,25,39,27,42,5,24,45,33,51,5,80,4,7,91,
       31,66,71,32,19,79,58,61,82,89,63,7,4,50,10,48,24,75,19,22,
       73,54,51,25,33,20,52,79,97,70,54,63,49};    
@@ -34,4 +83,5 @@ public class Merge
     }
     System.out.println("SUCCESS!");
   }
+
 }
